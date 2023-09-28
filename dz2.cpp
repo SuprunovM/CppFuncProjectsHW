048562fc68572969454e319dd5b45b62808b36fb
SuprunovM <144795321+SuprunovM@users.noreply.github.com>
2023-09-28 18:03:19 +0300

    Add files via upload

---
+++ dz2.cpp
@@ -0,0 +1,46 @@
+﻿//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    float r;
+//    cout << "r circle: ";
+//    cin >> r;
+//
+//    cout << "S circle: " << r * r * 3.14 << "\n";
+//
+//    cout << "Circumference: " << r * 6.28 << "\n";
+//
+//    cout << "Circle diameter: " << r * 2 << "\n";
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    float a, b, c, d;
+//    cout << "Student grade for 1st quarter: ";
+//    cin >> a;
+//    cout << "Student grade for 1st quarter: ";
+//    cin >> b;
+//    cout << "Student grade for 1st quarter: ";
+//    cin >> c;
+//    cout << "Student grade for 1st quarter: ";
+//    cin >> d;
+//    cout << "Student's assessment for the year: " << round((a + b + c + d) / 4);
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    float a, b, c;
+//    cout << "Salary for 1st month: ";
+//    cin >> a;
+//    cout << "Salary for 1st month: ";
+//    cin >> b;
+//    cout << "Salary for 1st month: ";
+//    cin >> c;
+//    cout << "Salary for the quarter " << a + b + c;
+//}
