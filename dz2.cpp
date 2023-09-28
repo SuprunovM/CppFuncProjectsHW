c5b7bb263ed559c87ad84689806890f3680d1c15
SuprunovM <144795321+SuprunovM@users.noreply.github.com>
2023-09-28 18:14:00 +0300

    Add files via upload

---
+++ dz2.cpp
@@ -0,0 +1,18 @@
+﻿﻿#include <iostream>
+
+int main()
+{
+    int a;
+    using namespace std;
+    cout << "Enter a six-digit number: \n";
+    cin >> a;
+    if (a / 100000 == 0 || a / 1000000 > 0) {
+        cout << "Error";
+    }
+    else if ((a / 100000) + (a / 10000) % 10 + (a / 1000) % 10 == (a / 100) % 10 + (a / 10) % 10 + (a) % 10) {
+        cout << "Given number is lucky";
+    }
+    else {
+        cout << "The given number is unlucky";
+    }
+}
