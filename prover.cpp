a378c5f92df528454b80ccb3e348c0cd24ed8275
SuprunovM <144795321+SuprunovM@users.noreply.github.com>
2023-12-26 13:42:09 +0300

    Add files via upload

---
+++ prover.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	// 1
+	srand(time(NULL));
+	//int a, b, c, d;
+	//cin >> a >> b >> c >> d;
+	//if (a > b && a > c && a > d) cout << a;
+	//if (b > a && b > c && b > d) cout << b;
+	//if (c > a && c > b && c > d) cout << c;
+	//if (d > a && d > b && d > c) cout << d;
+
+	// 2
+	
+	//int a, b, n;
+	//cout << "Vvedite diapozon: ";
+	//cin >> n;
+	//cin >> a >> b;
+	//if (a < b) swap(a, b);
+	//for (int i = 0; i < n; i++) {
+	//	cout << a << " ";
+	//	a++;
+	//}
+
+	// 3
+
+	//float max;
+	//cin >> max;
+	//if (max < 0);
+	//for (float i = 0; i <= max; i += 0.5) {
+	//	cout << i << " ";
+	//}
+
+	// 4
+
+	//int a;
+	//float summ = 0, count = 0;
+	//do {
+	//	cin >> a;
+	//	summ += a;
+	//	count++;
+	//} while (a != 0);
+	//if (a == 0) {
+	//	cout << summ / (count - 1) << "arifm";
+	//}
+
+	// 5
+
+	//char a;
+	//cin >> a;
+	//if (a >= 'a' && a <= 'z') {
+	//	cout << "angl bukva";
+	//}
+	//else if (a >= '0' && a <= '9') {
+	//	cout << "chislo";
+	//}
+	//else if (a >= '!' && a <= '~') {
+	//	cout << "simvol";
+	//}
+	//else {
+	//	cout << "neizvestniy simvol ili russkaya bukva";
+	//}
+
+	// 6-7
+
+	//int arr[10];
+	//int arr2[10];
+	//for (int i = 0; i < 10; i++) {
+	//	arr[i] = 7 + rand() % 18 - 7 + 1;
+	//	cout << arr[i] << " ";
+	//}
+
+	//cout << endl;
+
+	//for (int i = 0; i < 10; i++) {
+	//	if (arr[i] % 2 == 0) {
+	//		arr2[i] = arr[i];
+	//		cout << arr2[i] << " ";
+	//	}
+	//}
+
+	// 8
+
+
+}
+
+
+// из 10
